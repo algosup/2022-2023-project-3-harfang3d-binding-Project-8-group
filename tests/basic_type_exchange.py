906a2e96b25af87c6043eff0104648ddb64cf60f
leo-chartier <leo.chartier@algosup.com>
2023-02-07 12:17:18 +0100

    rename test files as original

---
+++ tests/basic_type_exchange.py
@@ -0,0 +1,122 @@
+import lib
+
+
+def bind_test(gen):
+	gen.start('my_test')
+
+	lib.bind_defaults(gen)
+
+	# inject test code in the wrapper
+	gen.insert_code('''\
+// basic interoperability
+int return_int() { return 8; }
+float return_float() { return 8.f; }
+const char *return_const_char_ptr() { return "const char * -> string"; }
+
+static int static_int = 9;
+
+int *return_int_by_pointer() { return &static_int; }
+int &return_int_by_reference() { return static_int; }
+
+// argument passing
+int add_int_by_value(int a, int b) { return a + b; }
+int add_int_by_pointer(int *a, int *b) { return *a + *b; }
+int add_int_by_reference(int &a, int &b) { return a + b; }
+\n''', True, False)
+
+	gen.add_include('string', True)
+
+	gen.bind_function('return_int', 'int', [])
+	gen.bind_function('return_float', 'float', [])
+	gen.bind_function('return_const_char_ptr', 'const char *', [])
+
+	gen.bind_function('return_int_by_pointer', 'int*', [])
+	gen.bind_function('return_int_by_reference', 'int&', [])
+
+	gen.bind_function('add_int_by_value', 'int', ['int a', 'int b'])
+	gen.bind_function('add_int_by_pointer', 'int', ['int *a', 'int *b'])
+	gen.bind_function('add_int_by_reference', 'int', ['int &a', 'int &b'])
+
+	gen.finalize()
+	return gen.get_output()
+
+
+test_python = '''\
+import my_test
+
+assert my_test.return_int() == 8
+assert my_test.return_float() == 8
+assert my_test.return_const_char_ptr() == "const char * -> string"
+
+assert my_test.return_int_by_pointer() == 9
+assert my_test.return_int_by_reference() == 9
+
+assert my_test.add_int_by_value(3, 4) == 7
+assert my_test.add_int_by_pointer(3, 4) == 7
+assert my_test.add_int_by_reference(3, 4) == 7
+'''
+
+test_lua = '''\
+my_test = require "my_test"
+
+assert(my_test.return_int() == 8)
+assert(my_test.return_float() == 8)
+assert(my_test.return_const_char_ptr() == "const char * -> string")
+
+assert(my_test.return_int_by_pointer() == 9)
+assert(my_test.return_int_by_reference() == 9)
+
+assert(my_test.add_int_by_value(3, 4) == 7)
+assert(my_test.add_int_by_pointer(3, 4) == 7)
+assert(my_test.add_int_by_reference(3, 4) == 7)
+'''
+
+
+test_go = '''\
+package mytest
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+// Test ...
+func Test(t *testing.T) {
+	assert.Equal(t, ReturnInt(), 8, "should be the same.")
+	assert.Equal(t, ReturnFloat(), float32(8), "should be the same.")
+	assert.Equal(t, ReturnConstCharPtr(), "const char * -> string", "should be the same.")
+
+	assert.Equal(t, *ReturnIntByPointer(), 9, "should be the same.")
+	assert.Equal(t, *ReturnIntByReference(), 9, "should be the same.")
+	
+	assert.Equal(t, AddIntByValue(3, 4), 7, "should be the same.")
+	a := int32(3)
+	b := int32(4)
+	assert.Equal(t, AddIntByPointer(&a, &b), 7, "should be the same.")
+	assert.Equal(t, AddIntByReference(&a, &b), 7, "should be the same.")
+}
+'''
+test_rust = '''\
+include!("bindings.rs");
+
+#[test]
+fn test() {
+    unsafe {
+        assert_eq!(my_test_return_int(), 8);
+        assert_eq!(my_test_return_float(), 8.0);
+        
+        let c_char_ptr = my_test_return_const_char_ptr();
+        let c_string = std::ffi::CStr::from_ptr(c_char_ptr).to_str().unwrap();
+        assert_eq!(c_string, "const char * -> string");
+
+        assert_eq!(*my_test_return_int_by_pointer(), 9);
+        assert_eq!(*my_test_return_int_by_reference(), 9);
+
+        let mut a = 3;
+        let mut b = 4;
+        assert_eq!(my_test_add_int_by_value(a, b), 7);
+        assert_eq!(my_test_add_int_by_pointer(&mut a, &mut b), 7);
+        assert_eq!(my_test_add_int_by_reference(&mut a, &mut b), 7);
+    }
+}
+'''
---
+++ tests/enumeration.py
@@ -0,0 +1,120 @@
+import lib
+
+
+def bind_test(gen):
+	gen.start('my_test')
+
+	lib.bind_defaults(gen)
+
+	# inject test code in the wrapper
+	gen.insert_code('''\
+enum GlobalEnum { GE_a, GE_b, GE_c = 8 };
+
+struct Type {
+	enum StructEnum { SE_a, SE_b = 128, SE_c = 512 };
+};
+
+enum TypedEnum : int16_t { TE_a, TE_b, TE_c = 16384 };
+
+enum NamedEnum { NE_a, NE_b, NE_c = 4096 };
+''', True, False)
+
+	gen.bind_named_enum('GlobalEnum', ['GE_a', 'GE_b', 'GE_c'])
+	gen.bind_named_enum('Type::StructEnum', ['SE_a', 'SE_b', 'SE_c'])
+	gen.bind_named_enum('TypedEnum', ['TE_a', 'TE_b', 'TE_c'], storage_type='int16_t')
+	gen.bind_named_enum('NamedEnum', ['NE_a', 'NE_b', 'NE_c'])
+
+	gen.finalize()
+	return gen.get_output()
+
+
+test_python = '''\
+import my_test
+
+assert my_test.GE_a == 0
+assert my_test.GE_b == 1
+assert my_test.GE_c == 8
+
+assert my_test.SE_a == 0
+assert my_test.SE_b == 128
+assert my_test.SE_c == 512
+
+assert my_test.TE_a == 0
+assert my_test.TE_b == 1
+assert my_test.TE_c == 16384
+
+assert my_test.NE_a == 0
+assert my_test.NE_b == 1
+assert my_test.NE_c == 4096
+'''
+
+test_lua = '''\
+my_test = require "my_test"
+
+assert(my_test.GE_a == 0)
+assert(my_test.GE_b == 1)
+assert(my_test.GE_c == 8)
+
+assert(my_test.SE_a == 0)
+assert(my_test.SE_b == 128)
+assert(my_test.SE_c == 512)
+
+assert(my_test.TE_a == 0)
+assert(my_test.TE_b == 1)
+assert(my_test.TE_c == 16384)
+
+assert(my_test.NE_a == 0)
+assert(my_test.NE_b == 1)
+assert(my_test.NE_c == 4096)
+'''
+
+test_go = '''\
+package mytest
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+// Test ...
+func Test(t *testing.T) {
+	assert.Equal(t, GEa, GlobalEnum(0), "should be the same.")
+	assert.Equal(t, GEb, GlobalEnum(1), "should be the same.")
+	assert.Equal(t, GEc, GlobalEnum(8), "should be the same.")
+
+	assert.Equal(t, SEa, StructEnum(0), "should be the same.")
+	assert.Equal(t, SEb, StructEnum(128), "should be the same.")
+	assert.Equal(t, SEc, StructEnum(512), "should be the same.")
+
+	assert.Equal(t, TEa, TypedEnum(0), "should be the same.")
+	assert.Equal(t, TEb, TypedEnum(1), "should be the same.")
+	assert.Equal(t, TEc, TypedEnum(16384), "should be the same.")
+
+	assert.Equal(t, NEa, NamedEnum(0), "should be the same.")
+	assert.Equal(t, NEb, NamedEnum(1), "should be the same.")
+	assert.Equal(t, NEc, NamedEnum(4096), "should be the same.")
+}
+'''
+
+test_rust = '''\
+include!("bindings.rs");
+
+#[test]
+fn test() {
+	assert_eq!(GE_a, 0);
+	assert_eq!(GE_b, 1);
+	assert_eq!(GE_c, 8);
+
+	assert_eq!(SE_a, 0);
+	assert_eq!(SE_b, 128);
+	assert_eq!(SE_c, 512);
+
+	assert_eq!(TE_a, 0);
+	assert_eq!(TE_b, 1);
+	assert_eq!(TE_c, 16384);
+
+	assert_eq!(NE_a, 0);
+	assert_eq!(NE_b, 1);
+	assert_eq!(NE_c, 4096);
+}
+'''
